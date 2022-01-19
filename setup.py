aca3da05df4785e299c07a40409cb27a59fb46ad
msefelix <felix@DESKTOP-6LM1S5B.localdomain>
2022-01-20 10:06:54 +1100

    init setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(name="cofli", 
+version="1.0.0", 
+author="Feng Li", 
+author_email="feng.li.vic@gmail.com",
+description="COVID analytics and prediction in AUS VIC",
+url="https://github.com/msefelix/covid.git",
+packages=setuptools.find_packages(),
+python_requires=">=3.7.4")
