1dcf748e8440d840faea9d573e3fa8dd67854c01
msefelix <felix@DESKTOP-6LM1S5B.localdomain>
2022-01-23 16:47:06 +1100

    cloud func to consolidate vic data; func to update gpd

---
+++ cofli/data/prepare_geo.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import geopandas as gpd
+folder = "gs://covid-analytics-data/data/vic/gov"
+
+def prepare_vic_post(df: pd.DataFrame):
+    # Load Geo data for postcode from source1
+    gdf1 = gpd.read_file(f"{folder}/data/geo/1270055003_poa_2016_aust_shape.zip")
+    gdf1['POA_CODE16'] = gdf1['POA_CODE16'].astype(int)
+    gdf1 = gdf1.query("POA_CODE16 >= 3000 and POA_CODE16 < 4000")
+
+    # Load Geo data for postcode from source2
+    gdf2 = gpd.read_file(f"{folder}/data/geo/VMADMIN")
+    gdf2['POSTCODE'] = gdf2['POSTCODE'].astype(int)
+    gdf2 = gdf2.query("POSTCODE >= 3000 and POSTCODE < 4000")
+
+    # Check missing postcode
+    print(set(df['postcode']) - set(gdf1['POA_CODE16']))
+    print(set(df['postcode']) - set(gdf2['POSTCODE']))
+
+    # Combine
+    gdf1 = gdf1.rename(columns={'POA_CODE16':'postcode'})[['postcode', 'geometry']]
+    gdf2 = gdf2.query("POSTCODE == 3336").rename(columns={'POSTCODE':'postcode'})[['postcode', 'geometry']]
+    gdf = pd.concat([gdf1, gdf2], axis=0, ignore_index=True)
+    print(set(df['postcode']) - set(gdf2['POSTCODE']))
+    print(set(gdf['postcode']) - set(df['postcode']))
+
+    # Save
+    gdf.to_file(f"{folder}/data/geo/vic/postcode.shp")
+
+    return
---
+++ cofli/data/update_vic_gov.py
@@ -0,0 +1,66 @@
+import requests
+import gcsfs
+import pandas as pd
+from datetime import date
+folder = "gs://covid-analytics-data/data/vic/gov"
+today = str(date.today())
+
+
+def fetch_url_to_gcs(fs, url: str, opath: str):
+    """Download a file from the URL and save it at opath on GCS.
+
+    Args:
+        url (str): [description]
+        opath (str): [description]
+    """    
+    response = requests.get(url)
+    with fs.open(opath, 'wb') as f:
+        f.write(response.content)
+    return
+
+
+def vic_by_location():
+    fs = gcsfs.GCSFileSystem()
+    
+    vic_active_by_gov = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9oKYNQhJ6v85dQ9qsybfMfc-eaJ9oKVDZKx-VGUr6szNoTbvsLTzpEaJ3oW_LZTklZbz70hDBUt-d/pub?gid=0&single=true&output=csv"
+    fetch_url_to_gcs(fs, vic_active_by_gov, f"{folder}/vic_active_by_gov_{today}.csv")
+
+    vic_active_by_post = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTwXSqlP56q78lZKxc092o6UuIyi7VqOIQj6RM4QmlVPgtJZfbgzv0a3X7wQQkhNu8MFolhVwMy4VnF/pub?gid=0&single=true&output=csv"
+    fetch_url_to_gcs(fs, vic_active_by_post, f"{folder}/vic_active_by_post_{today}.csv")
+
+    return
+
+
+def process_a_post(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.drop_duplicates().dropna(subset=['postcode'])
+    df['postcode'] = df['postcode'].fillna(9999).astype(int)
+    df['population'] = df['population'].fillna(0)
+
+    cols = ['postcode', 'population', 'active', 'cases', 'new', 'data_date', 'file_processed_date']
+    df = df[cols]
+
+    df['data_date'] = pd.to_datetime(df['data_date'], yearfirst=False)
+    df['file_processed_date'] = pd.to_datetime(df['file_processed_date'], yearfirst=True)
+
+    df['active pop %'] = (df['active'] / df['population'] * 100).round(1)
+    df['approximate infected pop %'] = (df['cases'] / df['population'] * 100).round(1)
+
+    return df
+
+
+def add_a_csv(filename='post'):
+    df_new = pd.read_csv(f"{folder}/vic_active_by_{filename}_{today}.csv")
+    df_new = process_a_post(df_new)
+
+    df_old = pd.read_parquet(f"{folder}/data/vic/cases_{filename}.parquet")
+
+    df = pd.concat([df_old, df_new], axis=0, ignore_index=True)
+    df.to_parquet(f"{folder}/data/vic/cases_{filename}.parquet")
+    return
+
+
+def main(event, context):
+    vic_by_location()
+    add_a_csv()
+
+    return
