16547be298f1ec794cad47c241cfd9a06aebd6e1
msefelix <felix@DESKTOP-6LM1S5B.localdomain>
2022-02-04 14:45:03 +1100

    save covidlive data for all locations

--- cofli/data/cf_update_covidlive.py
+++ cofli/data/cf_update_covidlive.py
@@ -1,29 +1,46 @@
 import pandas as pd
-from cofli.settings import bucket
+from typing import Dict
+from cofli.settings import locations
 
 
-def update_ts_by_state(state:str) -> pd.DataFrame:
-    ### Download data
+def download_ts_by_location(location:str) -> Dict[str, pd.DataFrame]:
     live_ts = {}
     for ts_type, cols in {'cases':['NEW'],
                         'active-cases':['ACTIVE'],
                         'tests':['NET'],
                         'hospitalised':['HOSP', 'ICU', 'VENT']}.items():
-        df_temp = pd.read_html(f"https://covidlive.com.au/report/daily-{ts_type}/{state}")[1][['DATE'] + cols]
+        df_temp = pd.read_html(f"https://covidlive.com.au/report/daily-{ts_type}/{location}")[1][['DATE'] + cols]
         live_ts[ts_type] = df_temp
-        
-    ### Consolidate
+    
+    return live_ts
+
+
+def consolidate_ts(live_ts: Dict[str, pd.DataFrame]) -> pd.DataFrame:
     res = []
-    for ts_type, df_temp in live_ts.items():
+    for df_temp in live_ts.values():
         df_temp.columns = [x.lower() for x in df_temp.columns]
         df_temp['date'] = pd.to_datetime(df_temp['date'], yearfirst=False)
         res.append(df_temp.set_index('date'))
     
     res = pd.concat(res, axis=1, ignore_index=False).dropna(how='all').rename(columns={'net':'tests'})
+    
+    for col in ['new', 'tests', 'active']:
+        if res[col].dtype == 'O':
+            res[col] = res[col].replace(to_replace={'-':0})
+        res[col] = res[col].fillna(0).astype(int)
 
     res['active (k)'] = (res['active'] / 1000)
     res['tests (k)'] = (res['tests'] / 1000)
 
-    res = res.drop(['cases', 'active', 'tests'], axis=1).fillna(0).astype(int).reset_index()
+    res = res.drop(['active', 'tests'], axis=1).fillna(0).astype(int).reset_index()
         
     return res
+
+
+def update_ts(opath):
+    for location in locations:
+        print(location)
+        live_ts = download_ts_by_location(location)
+        ts_df = consolidate_ts(live_ts)
+        ts_df.to_parquet(f"{opath}/{location}.parquet")
+    return
--- cofli/settings.py
+++ cofli/settings.py
@@ -1,3 +1,4 @@
 from datetime import date
 today = str(date.today())
 bucket = "gs://covid-analytics-data"
+locations = ['aus', 'vic', 'nsw', 'qld', 'wa', 'sa', 'act', 'tas', 'nt']
