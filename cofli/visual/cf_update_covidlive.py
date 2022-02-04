21e69d0c33f00427780871e4a58fa4b028121772
msefelix <felix@DESKTOP-6LM1S5B.localdomain>
2022-02-04 22:00:14 +1100

    change figure col name

--- cofli/visual/cf_update_covidlive.py
+++ cofli/visual/cf_update_covidlive.py
@@ -13,7 +13,7 @@ fig_types = {'new':'Daily New Cases (PCR + RAT)',
 
 
 def make_a_ts_fig(df: pd.DataFrame, y:str, title:str):
-    cols = ['On the day', '7D AVG']
+    cols = ['RAW', '7D AVG']
     df_temp = df[[y, f"7D AVG - {y}"]]
     df_temp.columns = cols
     fig = px.line(df_temp, x=df_temp.index, y=cols, 
