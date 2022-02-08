772a744ca3c4779cf539ee69d6550964a1171574
msefelix <felix@DESKTOP-6LM1S5B.localdomain>
2022-02-08 21:49:47 +1100

    add simplified requirements.txt to repo

--- cofli/settings.py
+++ cofli/settings.py
@@ -2,3 +2,25 @@ from datetime import date
 today = str(date.today())
 bucket = "gs://covid-analytics-data"
 locations = ['aus', 'vic', 'nsw', 'qld', 'wa', 'sa', 'act', 'tas', 'nt']
+
+# active imported libs
+# cofli
+# dash
+# dash_bootstrap_components
+# gcsfs
+# geopandas
+# pandas
+# pickle
+# plotly
+# requests
+# tempfile
+# zipfile
+
+# dash==2.1.0
+# dash-bootstrap-components==1.0.2
+# gcsfs==2022.1.0
+# geopandas==0.9.0
+# pandas==1.1.4
+# plotly==5.5.0
+# requests==2.25.1
+# git+https://github.com/msefelix/covid.git
