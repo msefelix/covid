89c88ae2c4b3877910db584043a53f6b45bcce46
msefelix <felix@DESKTOP-6LM1S5B.localdomain>
2022-02-12 11:27:46 +1100

    move bucket to env var

--- cofli/settings.py
+++ cofli/settings.py
@@ -1,11 +1,16 @@
+import os
 from datetime import date
 today = str(date.today())
+
 # Local Docker Mode
 # bucket = './'
 # Local virtualenv mode
 # bucket = "/home/felix/learning/covid_aus"
 # GCS mode
-bucket = "gs://covid-analytics-data"
+# bucket = "gs://covid-analytics-data"
+# Get value from env var
+bucket = os.getenv('BUCKET')
+
 locations = ['aus', 'vic', 'nsw', 'qld', 'wa', 'sa', 'act', 'tas', 'nt']
 
 # active imported libs
